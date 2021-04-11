b2bf38e268fa02b8e7b604c8576ef125317a5551
fantasmo <gheadley27@gmail.com>
2021-04-11 22:06:48 +0100

    very simple dry run capability

--- dlrippyr.py
+++ dlrippyr.py
@@ -9,11 +9,32 @@ Description: A CLI utility for encoding video files
 import click
 
 
+def handbrake_cmd(input_file, output_file, start=0, stop=0):
+    cmd = ''
+    in_ = '-i {} '.format(input_file)
+    out_ = '-o {}'.format(output_file)
+    start_ = ''
+    stop_ = ''
+    if start:
+        start_ = '--start-at seconds:{} '.format(start)
+    if stop:
+        stop_ = '--start-at seconds:{} '.format(stop)
+    if (start_ or stop_):
+        cmd = in_ + start_ + stop_ + out_
+    else:
+        cmd = in_ + out_
+    return cmd
+
+
 @click.command()
-@click.argument('name')
-def cli(name):
+@click.argument('input_file')
+@click.option('-o', 'output_file', default='test.mkv')
+@click.option('--start', 'start', default=0)
+@click.option('--stop', 'stop', default=0)
+def cli(input_file, output_file, start, stop):
     """
     A tool for video encoding using HandBrakeCLI. Accepts an input file to be
     reencoded
     """
-    click.echo('Hello, {}!'.format(name))
+    cmd = handbrake_cmd(input_file, output_file, start, stop)
+    click.echo('Your handbrake dry run is:\nhandbrake {}'.format(cmd))
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dlrippyr',
-    version='0.0.1',
+    version='0.1.0',
     py_modules=['dlrippyr'],
     install_requires=[
         'click',

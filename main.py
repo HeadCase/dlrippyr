1d45ed4aca4346695323e2036949f26bcf4b307a
fantasmo <gheadley27@gmail.com>
2021-04-11 15:38:01 +0100

    skeleton

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+File: main.py
+Author: G. Headley
+Email: gheadley27@gmail.com
+Github: https://github.com/headcase
+Description: A CLI utility for encoding video files
+"""
+
+
+def main():
+    """main function"""
+    print('Hello, world!')
+
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='',
+    version='0.1.0',
+    py_modules=['dlrippyr'],
+    install_requires=[
+        'click'
+    ]
+)

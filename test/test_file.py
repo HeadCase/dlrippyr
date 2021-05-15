9e7cb8f0b091db7c7eeb294b48f2cd2de07332fa
fantasmo <gheadley27@gmail.com>
2021-05-15 14:28:55 +0100

    basic file input tests

--- test/test_file.py
+++ test/test_file.py
@@ -1,23 +1,31 @@
 #!/usr/bin/env python
 from pathlib import Path
 
-from dlrippyr import features
+from dlrippyr import features, utils
 
 TEST_FILE = Path('samples/Wentworth.mov')
+DEFAULT_OUT = 'samples/Wentworth.mkv'
+TEST_OUT = Path('samples/Wentworth.mp4')
 PRESET = 'conf/x265-1080p-mkv.json'
 
 
-def test_file_dry_run(capsys):
-    features.run_dry(TEST_FILE, '', PRESET, None, None)
-    out, err = capsys.readouterr()
-    assert out == 'HandBrakeCLI --preset-import-file conf/x265-1080p-mkv.json'\
-    ' -Z conf/x265-1080p-mkv -i samples/Wentworth.mov -o'\
-    ' samples/Wentworth.mkv\n'
-    assert err == ''
+def test_file_make_hb():
+    dry_no_out = features.make_handbrake(TEST_FILE, '', PRESET, None, None)
+    dry_out = features.make_handbrake(TEST_FILE, TEST_OUT, PRESET, None, None)
+
+    cmd_no_out = utils.make_cmd(TEST_FILE, DEFAULT_OUT, PRESET, None, None)
+    cmd_out = utils.make_cmd(TEST_FILE, TEST_OUT, PRESET, None, None)
+
+    assert dry_no_out == [cmd_no_out]
+    assert dry_out == [cmd_out]
 
 
 def test_file_sample():
-    pass
+    dry_out = features.make_handbrake(TEST_FILE, '', PRESET, 0, 20)
+
+    cmd_out = utils.make_cmd(TEST_FILE, DEFAULT_OUT, PRESET, 0, 20)
+
+    assert dry_out == [cmd_out]
 
 
 def test_file_get_info():

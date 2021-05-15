f8ffe1d9510edc96760428d6f6bde3edcda78ee7
fantasmo <gheadley27@gmail.com>
2021-05-15 22:47:32 +0100

    dummy dir with real video files; fixed cli input to take unlimited args

--- dlrippyr/cli.py
+++ dlrippyr/cli.py
@@ -17,7 +17,7 @@ DEFAULT_PRESET = 'conf/x265-1080p-mkv.json'
 
 
 @click.command()
-@click.argument('input-file', type=click.Path(exists=True))
+@click.argument('args', nargs=-1, type=click.Path(exists=True))
 @click.version_option()
 @click.option('-o',
               '--output-path',
@@ -50,38 +50,39 @@ DEFAULT_PRESET = 'conf/x265-1080p-mkv.json'
               'stop',
               default=None,
               help='Time at which to stop encoding, default None')
-def cli(input_file, output_path, start, stop, info, sample, preset, dry_run):
+def cli(args, output_path, start, stop, info, sample, preset, dry_run):
     """
     A tool for encoding AVC (H264) video files to the more space-efficient
     HEVC (H265) codec using HandBrakeCLI. Accepts a single input video file or
     a directory or tree
     """
 
-    input_path = Path(input_file)
-    # Output does not need to be converted to a Path as it is only ever
-    # supplied as a string to HandBrakeCLI
+    for input_file in args:
+        input_path = Path(input_file)
+        # Output does not need to be converted to a Path as it is only ever
+        # supplied as a string to HandBrakeCLI
 
-    # Currently bundling command-line opts/args into a dict for parsing. What
-    # is the smarter way to do this?
-    user_input = {
-        'input': input_path,
-        'output': output_path,
-        'start': start,
-        'stop': stop,
-        'info': info,
-        'sample': sample,
-        'preset': preset,
-        'dry run': dry_run,
-        'test': False
-    }
-    # User can supply a directory, which we then parse for any video files
-    if input_path.is_dir():
-        if output_path:
-            raise RuntimeError('Supplying an output file is not supported '
-                               'for directories')
+        # Currently bundling command-line opts/args into a dict for parsing.
+        # What is the smarter way to do this?
+        user_input = {
+            'input': input_path,
+            'output': output_path,
+            'start': start,
+            'stop': stop,
+            'info': info,
+            'sample': sample,
+            'preset': preset,
+            'dry run': dry_run,
+            'test': False
+        }
+        # User can supply a directory, which we then parse for any video files
+        if input_path.is_dir():
+            if output_path:
+                raise RuntimeError('Supplying an output file is not supported '
+                                   'for directories')
+            else:
+                parse_user_input(user_input)
+
+        # User can supply single video file, which we process directly
         else:
             parse_user_input(user_input)
-
-    # User can supply single video file, which we process directly
-    else:
-        parse_user_input(user_input)
